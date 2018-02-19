f4e13e15d82dcbb3701c146179928fee4f5d28d0
Masaya Kazama <miyan@t-p.jp>
2018-02-19 10:09:26 +0900

    Add preview mode in development

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,7 @@ module.exports = {
   env: {
     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
     CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
+    CTF_CPA_ACCESS_TOKEN: process.env.CTF_CPA_ACCESS_TOKEN,
     CTF_PERSON_ID: process.env.CTF_PERSON_ID,
     CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID
   },
--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,9 +1,11 @@
 const contentful = require('contentful')
 // use default environment config for convenience
 // these will be set via `env` property in nuxt.config.js
+const isDev = process.env.NODE_ENV === 'development'
 const config = {
   space: process.env.CTF_SPACE_ID,
-  accessToken: process.env.CTF_CDA_ACCESS_TOKEN
+  accessToken: isDev ? process.env.CTF_CPA_ACCESS_TOKEN : process.env.CTF_CDA_ACCESS_TOKEN,
+  host: isDev ? 'preview.contentful.com' : 'cdn.contentful.com'
 }
 
 // export `createClient` to use it in page components
